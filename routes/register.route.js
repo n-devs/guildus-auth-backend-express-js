ad95db05560a8806dc10192b774246695a311303
n-devs <n.devs260340@gmail.com>
2022-09-12 13:51:31 +0700

    Update register.route.js

--- routes/register.route.js
+++ routes/register.route.js
@@ -1,36 +1,57 @@
 var express = require("express");
 var router = express.Router();
 
-async function isRequest(req, res, next) {
+async function isRequire(req, res, next) {
   try {
     const { email, password, passwordConfirm } = req.body;
 
     if (!email) {
-      return res.send({
+      return res.status(401).send({
         statusCode: 401,
         message: "request not allowed because there is no email",
       });
     } else if (!password) {
-      return res.send({
+      return res.status(401).send({
         statusCode: 401,
         message: "request not allowed because there is no password",
       });
     } else if (!passwordConfirm) {
-      return res.send({
+      return res.status(401).send({
         statusCode: 401,
         message: "request not allowed because there is no password confirm",
       });
-    } else if (password !== passwordConfirm) {
-      return res.send({
-        statusCode: 401,
+    } else if(!email && !password && !passwordConfirm) {
+      return res.status(400).send({
+        statusCode: 400,
         message:
-          "The request is not allowed because the password and verification password do not match.",
+          "The server cannot or will not process the request due to something that is perceived to be a client error",
       });
-    } else {
+    }else {
       return next();
     }
   } catch (e) {
-    return  res.send({
+    return res.status(400).send({
+      statusCode: 400,
+      message:
+        "The server cannot or will not process the request due to something that is perceived to be a client error",
+    });
+  }
+}
+
+async function isValidation(req, res, next) {
+  try {
+    const { email } = req.body;
+    const _regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (_regex.test(email)) {
+      return next();
+    } else {
+      return res.status(401).send({
+        statusCode: 401,
+        message: "request not allowed because there is email match",
+      });
+    }
+  } catch (e) {
+    return res.status(400).send({
       statusCode: 400,
       message:
         "The server cannot or will not process the request due to something that is perceived to be a client error",
@@ -40,12 +61,22 @@ async function isRequest(req, res, next) {
 
 async function register(req, res, next) {
   try {
-    return res.send({
-      statusCode: 201,
-      message: "Created user ok!",
-    });
+    const { password, passwordConfirm } = req.body;
+
+    if (password !== passwordConfirm) {
+      return res.status(401).send({
+        statusCode: 401,
+        message:
+          "The request is not allowed because the password and verification password do not match.",
+      });
+    } else {
+      return res.status(201).send({
+        statusCode: 201,
+        message: "Created user ok!",
+      });
+    }
   } catch (e) {
-    return res.send({
+    return res.status(400).send({
       statusCode: 400,
       message:
         "The server cannot or will not process the request due to something that is perceived to be a client error",
@@ -53,6 +84,6 @@ async function register(req, res, next) {
   }
 }
 /* POST Register. */
-router.post("/register", isRequest, register);
+router.post("/register", isRequire, isValidation, register);
 
 module.exports = router;
